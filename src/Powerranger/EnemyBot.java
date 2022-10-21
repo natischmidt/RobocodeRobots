c584145a01598da3ffd1ecd3bf73d40db15a8b9a
@jimmy.brissman <jimmy.brissman@protonmail.com>
2022-10-21 11:37:21 +0200

    Uppdaterat vår gemensamma powerranger med ett tracker-system via EnemyBot samt lagt till smartFire funktionen som vi pratade om.
    Testat funktionerna mot Stefans original, och roboten presterar lite bättre med dessa funktioner.
    
    Pushar även upp en WIP-robot där jag testar experimentella funktioner innan jag pushar upp till den gemensamma. Svårt att veta om det är en förbättring om det inte är möjligt att testa mot den tidigare versionen och vi alltid uppdaterar den gemensamma. Plus att det kan vara bra att ha en backup, ifall något skulle gå snett med Git.

---
+++ src/Powerranger/EnemyBot.java
@@ -0,0 +1,63 @@
+package Powerranger;
+
+import robocode.ScannedRobotEvent;
+
+public class EnemyBot {
+
+    private String name;
+    private double bearing;
+    private double distance;
+    private double energy;
+    private double heading;
+    private double velocity;
+
+    public EnemyBot() {
+        reset();
+    }
+
+    public void reset(){
+        this.name = "";
+        this.bearing = 0.0;
+        this.distance = 0.0;
+        this.energy = 0.0;
+        this.heading = 0.0;
+        this.velocity = 0.0;
+    }
+
+    public void update(ScannedRobotEvent e){
+        this.name = e.getName();
+        this.bearing = e.getBearing();
+        this.distance = e.getDistance();
+        this.energy = e.getEnergy();
+        this.heading = e.getHeading();
+        this.velocity = e.getVelocity();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBearing() {
+        return bearing;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public double getHeading() {
+        return heading;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public boolean none(){
+        return name.equals("");
+    }
+}
