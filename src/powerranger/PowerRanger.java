3949c7cd1f16f0d0cb37666fe450270230bae06d
@jimmy.brissman <jimmy.brissman@protonmail.com>
2022-10-27 12:44:58 +0200

    Städat lite bland våra klassvariabler. Flyttade ihop de som hör ihop och kommenterade deras funktion.
    Justerade även i if-satsen för när vår robot ändrar från att köra längst med väggarna till att straf:a. Variabeln från statistiken går inte att använda eftersom denna redan innehåller data som är "inaktuell" vid tillfället. Lade till en ny "counter" som börjar när vi bara har en motståndare kvar, och döpte om variablerna som räknar totalen för att kunna skilja dessa åt.
    
    Strafe-metoden funkade inte heller riktigt som det är tänkt, för att dodge-metoden störde och vår radar blev förvirrad; så lade till en boolean för när vi strafar, och inaktiverar då dodge-mönstret.
    
    Vi hade även två variabler för att styra riktningen på vår robot nu när strafe-metoden är med, så döpte om dessa för att kunna skilja på vilken som är vilken. Dock kan vi ju ta bort den för walling, om vi ändå inte ska ändra riktningen vid något tillfälle.

--- src/powerranger/PowerRanger.java
+++ src/powerranger/PowerRanger.java
@@ -1,25 +1,34 @@
 package powerranger;
 
-import jimmy_l.Robotable;
 import robocode.AdvancedRobot;
 import robocode.BulletHitEvent;
 import robocode.ScannedRobotEvent;
 import robocode.util.Utils;
 import robocode.*;
-
 import java.awt.*;
 
 
 public class PowerRanger extends AdvancedRobot {
-    private EnemyBot currentTarget = new EnemyBot();
-    double shotBullet = 0;
-    double hitEnemybyBullet = 0;
-    double missedBullet = 0;
-    int dir = 1;
+    EnemyBot currentTarget = new EnemyBot();
+
+    // För statistiken i slutet av rundan
+    double totalBulletShot = 0;
+    double totalBulletHit = 0;
+    double totalBulletMiss = 0;
+
+    // Data för att ändra strategi vid 1 vs 1
+    boolean strafing = false;
+    double bulletMiss;
+    double bulletHit;
+    byte moveDirectionWhileStrafing = 1;
+
     double bulletPower = 1;
+
+    // Data för att bli mindre förutsägbar när vi rör oss längst med väggarna
+    byte moveDirectionWhileWalling = 1;
     double rand = 8;
     int timeToStop = 65;
-    private byte moveDirection = 1;
+
     boolean haveAlreadyGotAnEnergyBuddy = false;
     int redCounter = 1;
     int greenCounter = 90;
@@ -51,11 +60,21 @@ public class PowerRanger extends AdvancedRobot {
 
         while (true) {
             // Skanna igen om det behövs
-            setTurnRadarRight(Double.POSITIVE_INFINITY);
-            System.out.println("Scanna igen!!");
+            if(currentTarget.none())
+                setTurnRadarRight(Double.POSITIVE_INFINITY);
 
-            WallMovement();
-            DodgeMovment();
+            //Ifall endast en fiende är kvar och vi har mindre än 20 energi, eller...
+            if (getOthers() == 1 && (getEnergy() < 20 ||
+                    //vi har missad mer än 10 skott och missat mer än vad vi har träffat
+                    bulletMiss > bulletHit && bulletMiss > 10)){
+                strafing = true;
+                strafeEnemy();
+            }
+            else {
+                WallMovement();
+                DodgeMovment();
+            }
+            execute();
         }
     }
 
@@ -76,12 +95,12 @@ public class PowerRanger extends AdvancedRobot {
     public void WallMovement(){
         //Räknar ut hur jag ska åka längst väggarna
         if (Utils.isNear(getHeadingRadians(), 0D) || Utils.isNear(getHeadingRadians(), Math.PI)) {      //Utils.isNear returnerar true om differensen mellan de två argumenten är mindre än 1.0E-5, dvs 0.000010. I praktiken samma som == .Här betyder det true om vi är på väg (nästan) rakt norrut, eller (nästan) rakt söderut
-            ahead((Math.max(getBattleFieldHeight() - getY(), getY()) - 28) * dir);              //variabeln dir är alltid 1, men vi sparar den för nu, ifall vi vill använda den för att byta riktning.
-                                                                                                        //framåt (slagfältets höjd - vår y position) eller (vår y position - 28)
+            ahead((Math.max(getBattleFieldHeight() - getY(), getY()) - 28) * moveDirectionWhileWalling);              //variabeln dir är alltid 1, men vi sparar den för nu, ifall vi vill använda den för att byta riktning.
+            //framåt (slagfältets höjd - vår y position) eller (vår y position - 28)
         } else {
-            ahead((Math.max(getBattleFieldWidth() - getX(), getX()) - 28) * dir);
+            ahead((Math.max(getBattleFieldWidth() - getX(), getX()) - 28) * moveDirectionWhileWalling);
         }
-        turnRight(90 * dir);
+        turnRight(90 * moveDirectionWhileWalling);
     }
     public void DodgeMovment(){
         //Ändrar hur den åker
@@ -98,27 +117,22 @@ public class PowerRanger extends AdvancedRobot {
             }
         }
     }
-
-
-    public void onBulletMissed(BulletMissedEvent event) {
-        missedBullet++;
-    }
     public void onRoundEnded(RoundEndedEvent event) {
         Stats();
     }
 
     public void onScannedRobot(ScannedRobotEvent scannedRobot) {
         trackEnemy(scannedRobot);
-        //Ifall endast en fiende är kvar och vi har midnre 'n 20 energy eller att vi har missad mer 'n 10 skott byter vi stragtie
-        if (getOthers() == 1 && (int)getEnergy() < 20 || getOthers() == 1 && missedBullet > 10){
-            strafeEnemy();
-        }
+
         //data för att kunna sikta på fienden
         double absBearing = scannedRobot.getBearingRadians() + getHeadingRadians();
         double latVel = scannedRobot.getVelocity() * Math.sin(scannedRobot.getHeadingRadians() - absBearing);
         double radarTurn = absBearing - getRadarHeadingRadians();
+
         //Vill kalla den här ifrån också
-        DodgeMovment();
+        if(!strafing)
+            DodgeMovment();
+
         // Beräkna firepower
         smartFire();
 
@@ -131,19 +145,19 @@ public class PowerRanger extends AdvancedRobot {
     }
     private void strafeEnemy() {
         if (currentTarget.getDistance() < 200) {
-            // Back a little...
+            // Backa lite...
             setTurnRight(currentTarget.getBearing());
             setBack(200 - currentTarget.getDistance());
         } else if (currentTarget.getDistance() > 500) {
-            // Get closer...
+            // Kom närmre...
             setTurnRight(currentTarget.getBearing());
             setAhead(currentTarget.getDistance() - 200);
         }
         // Strafe...
         setTurnRight(currentTarget.getBearing() + 90);
-        setAhead(100 * moveDirection);
+        setAhead(100 * moveDirectionWhileStrafing);
         if (getTime() % 30 == 0)
-            moveDirection *= -1;
+            moveDirectionWhileStrafing *= -1;
     }
     private void smartFire() {
         if (getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 20) {
@@ -157,7 +171,7 @@ public class PowerRanger extends AdvancedRobot {
                         (currentTarget.getEnergy() / 4)));
 
             setFire(bulletPower);
-            shotBullet++;
+            totalBulletShot++;
         }
     }
 
@@ -193,15 +207,23 @@ public class PowerRanger extends AdvancedRobot {
     }
 
     public void onBulletHit(BulletHitEvent e) {
-        hitEnemybyBullet++;
+        totalBulletHit++;
+        if(getOthers() == 1)
+            bulletHit++;
+    }
+
+    public void onBulletMissed(BulletMissedEvent event) {
+        totalBulletMiss++;
+        if(getOthers() == 1)
+            bulletMiss++;
     }
 
     private void Stats() {
         out.print("________________Round ended_________________________");
-                        out.println("\nShots:" + shotBullet);
-                        out.println("Hits:" + hitEnemybyBullet);
-                        out.println("Misses:" + missedBullet);
-                        out.println("Accuracy:" + (hitEnemybyBullet / shotBullet));
+        out.println("\nShots:" + totalBulletShot);
+        out.println("Hits:" + totalBulletHit);
+        out.println("Misses:" + totalBulletMiss);
+        out.println("Accuracy:" + (totalBulletHit / totalBulletShot));
     }
     public void onCustomEvent(CustomEvent e) {
 
@@ -259,7 +281,7 @@ public class PowerRanger extends AdvancedRobot {
     public void energyBuddies (double energy, String name) {
 
         if (energy == getEnergy() && getEnergy() != 100 && getEnergy() != 0.0) {            //om scannad robot har lika mycket energi som vi och vår energi inte är 0 eller 100...
-                                                                                            //...så känner vi en djup samhörighet med den.
+            //...så känner vi en djup samhörighet med den.
             System.out.println("We both have " + (int)getEnergy() + " energy, " + currentTarget.getName() + ", we are energy buddies!");
             System.out.println("   ***     ***                   ***     ***                   ***     ***");
             System.out.println(" **   ** **   **               **   ** **   **               **   ** **   **");
@@ -291,9 +313,3 @@ public class PowerRanger extends AdvancedRobot {
     }
 
 }
-
-
-
-
-
-
